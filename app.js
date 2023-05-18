b24d39ec8b461acd9cb44ee1256b08efc3b00c45
dishant-852 <dishantgupta852@gmail.com>
2023-05-18 17:30:44 +0530

    second commit

--- app.js
+++ app.js
@@ -17,24 +17,32 @@ const appid="446911ac7fa80d718a2ead0ab2746670";
 const units="metric";
 const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+units+"&appid="+appid
 https.get(url,function(response){
-    response.on("data",function (data) {
-        const weatherdata=JSON.parse(data)
-        const temp=weatherdata.main.temp;
-        const description=weatherdata.weather[0].description
-        const icon=weatherdata.weather[0].icon
-        const imageurl="https://openweathermap.org/img/wn/"+ icon +"@2x.png"
-        res.write("<p>The weather in "+ query +" is "+description+".</p>");
-        res.write("<h1>The temprature in "+ query +" is " + temp + " Celcius .</h1>");
-        res.write("</br>")
-        res.write("<img src="+ imageurl +" >")
-        res.send();
-    })
+    if(response.statusCode!=200){
+        res.sendFile(__dirname+"/failure.html")
+    }
+    else{
+        response.on("data",function (data) {
+            const weatherdata=JSON.parse(data)
+            const temp=weatherdata.main.temp;
+            const description=weatherdata.weather[0].description
+            const icon=weatherdata.weather[0].icon
+            const imageurl="https://openweathermap.org/img/wn/"+ icon +"@2x.png"
+            res.write("<p>The weather in "+ query +" is "+description+".</p>");
+            res.write("<h1>The temprature in "+ query +" is " + temp + " Celcius .</h1>");
+            res.write("</br>")
+            res.write("<img src="+ imageurl +" >")
+            res.send();
+        })
+    }
+
 })
+
     
 })
 
-
-
+app.post("/failure",function(req,res){
+    res.redirect("/")
+})
 
 
 app.listen("3000", function (req, res) {
