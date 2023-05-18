e6bf0832124a33d82f10b84910981dd8d3b244fc
dishant-852 <dishantgupta852@gmail.com>
2023-05-18 17:01:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const https = require("https")
+const app = express();
+const bodyParser=require("body-parser");
+const { log } = require("console");
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",function(req,res){
+
+const query=req.body.cityName;
+const appid="446911ac7fa80d718a2ead0ab2746670";
+const units="metric";
+const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+units+"&appid="+appid
+https.get(url,function(response){
+    response.on("data",function (data) {
+        const weatherdata=JSON.parse(data)
+        const temp=weatherdata.main.temp;
+        const description=weatherdata.weather[0].description
+        const icon=weatherdata.weather[0].icon
+        const imageurl="https://openweathermap.org/img/wn/"+ icon +"@2x.png"
+        res.write("<p>The weather in "+ query +" is "+description+".</p>");
+        res.write("<h1>The temprature in "+ query +" is " + temp + " Celcius .</h1>");
+        res.write("</br>")
+        res.write("<img src="+ imageurl +" >")
+        res.send();
+    })
+})
+    
+})
+
+
+
+
+
+app.listen("3000", function (req, res) {
+    console.log("Server started on port 3000");
+})
